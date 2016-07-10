2fd33fa9c392d8a3d347b477f5dbfc0583cb6604
monicadivya <monicadivya9995@gmail.com>
2016-07-10 11:53:23 +0530

    Create pair.c

---
+++ pair.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,p,i,j;
+int count=0;
+clrscr();
+printf("\n Enter the number of students:");
+scanf("%d",&a);
+ p=2*a;
+for(i=0;i<p;i++)
+{
+for(j=i+1;j<=p;j++)
+ {
+ printf("\n%d\t%d\n",i,j);
+ count++;
+ }  }
+ printf("the daily walk combination is %d",count);
+ getch();
+}
